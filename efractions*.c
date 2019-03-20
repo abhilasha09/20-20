b80ef115c28aeb9c0b719e87e6e1a63c0beb928f
abhilasha09 <46891660+abhilasha09@users.noreply.github.com>
2019-03-20 18:33:42 +0530

    Update efractions*.c

--- efractions*.c
+++ efractions*.c
@@ -1,31 +1,35 @@
 #include<stdio.h>
 struct efrac
 {	
-int den[1000];
+	int den[1000];
+	int nden;
 };
 struct fraction
 {
 	int num,den;
 };
-void input(int n,int n1,struct efrac a[n1])
+void input(int n1,struct efrac a[n1])
 {
+	int n;
 	for(int j=0;j<n1;j++)
 	{
-	for(int i=0;i<n;i++)
+	printf("enter the no.of denominators for efrac %d",j+1);
+	scanf("%d",&a[j].nden);
+	for(int i=0;i<a[j].nden;i++)
 	{
 		printf("Enter denominator %d of egyptian fraction %d",i+1,j+1);
 		scanf("%d",&a[j].den[i]);
 	}
 	}
 }
-void compute(int n,int n1,struct efrac a[n1],struct fraction f[n1])
+void compute(int n1,struct efrac a[n1],struct fraction f[n1])
 {
-	int s;
+	int s,n;
 	for(int j=0;j<n1;j++)
 	{
 	f[j].num=0;
 	f[j].den=1;
-	for(int i=0;i<n;i++)
+	for(int i=0;i<a[j].nden;i++)
 	{
 		f[j].den=f[j].den*a[j].den[i];
 	}
@@ -34,7 +38,7 @@ void compute(int n,int n1,struct efrac a[n1],struct fraction f[n1])
 	}
 	for(int j=0;j<n1;j++)
 	{
-	for(int i=0;i<n;i++)
+	for(int i=0;i<a[j].nden;i++)
 	{
 		f[j].num=f[j].num+(f[j].den)/(a[j].den[i]);
 	}
@@ -57,19 +61,18 @@ void compute(int n,int n1,struct efrac a[n1],struct fraction f[n1])
 void output(int n1,struct fraction f[n1])
 {
 	for(int i=0;i<n1;i++)
-	printf("%d/%d",f[i].num,f[i].den);
+	printf("%d/%d\n",f[i].num,f[i].den);
 }
 int main()
 {
-	int n,n1;
+	int n1;
 	printf("Enter the number of egyptian fractions\n");
 	scanf("%d",&n1);
-	printf("enter the no.of denominators");
-	scanf("%d",&n);
+	
 	struct efrac a[n1];
 	struct fraction f[n1];
-	input(n,n1,a);
-    	compute(n,n1,a,f);
+	input(n1,a);
+    	compute(n1,a,f);
 	output(n1,f);
 	return 0;
 }
