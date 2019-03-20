ad7e96861971a1c6c8d8aa880c41b5318e91e140
abhilasha09 <46891660+abhilasha09@users.noreply.github.com>
2019-03-20 18:11:03 +0530

    Update efractions*.c

--- efractions*.c
+++ efractions*.c
@@ -1,57 +1,75 @@
 #include<stdio.h>
 struct efrac
-{
-	int den[1000];
+{	
+int den[1000];
 };
 struct fraction
 {
 	int num,den;
 };
-void input(int n,struct efrac *a)
+void input(int n,int n1,struct efrac a[n1])
 {
+	for(int j=0;j<n1;j++)
+	{
 	for(int i=0;i<n;i++)
 	{
-		printf("Enter denominator of egyptian fraction %d",i+1);
-		scanf("%d",&a->den[i]);
+		printf("Enter denominator %d of egyptian fraction %d",i+1,j+1);
+		scanf("%d",&a[j].den[i]);
+	}
 	}
 }
-void compute(int n,struct efrac a,struct fraction *f)
+void compute(int n,int n1,struct efrac a[n1],struct fraction f[n1])
 {
-	f->num=0;
-	f->den=1;
-	for(int i=0;i<n;i++)
-	{
-		f->den=f->den*a.den[i];
-	}
-	for(int i=0;i<n;i++)
-	{
-		f->num=f->num+(f->den)/(a.den[i]);
-	}
 	int s;
-	s=f->den;
-	for(int i=1;i<=s;i++)
+	for(int j=0;j<n1;j++)
 	{
-		if((f->num%i==0)&&(f->den%i==0))
-		{
-			f->num=f->num/i;
-			f->den=f->den/i;
-		}
+	f[j].num=0;
+	f[j].den=1;
+	for(int i=0;i<n;i++)
+	{
+		f[j].den=f[j].den*a[j].den[i];
 	}
 	
+	s=f[j].den;
+	}
+	for(int j=0;j<n1;j++)
+	{
+	for(int i=0;i<n;i++)
+	{
+		f[j].num=f[j].num+(f[j].den)/(a[j].den[i]);
+	}
+	}
+	
+	
+	for(int j=0;j<n1;j++)
+	{
+	for(int i=1;i<=s;i++)
+	{
+		
+		if((f[j].num%i==0)&&(f[j].den%i==0))
+		{
+			f[j].num=f[j].num/i;
+			f[j].den=f[j].den/i;
+		}
+	}
+	}
 }
-void output(struct fraction f)
+void output(int n1,struct fraction f[n1])
 {
-	printf("%d/%d",f.num,f.den);
+	for(int i=0;i<n1;i++)
+	printf("%d/%d",f[i].num,f[i].den);
 }
 int main()
 {
-	int n;
-	printf("enter the no.of egyptian fractions");
+	int n,n1;
+	printf("Enter the number of egyptian fractions\n");
+	scanf("%d",&n1);
+	printf("enter the no.of denominators");
 	scanf("%d",&n);
-	struct efrac a;
-	struct fraction f;
-	input(n,&a);
-    	compute(n,a,&f);
-	output(f);
+	struct efrac a[n1];
+	struct fraction f[n1];
+	input(n,n1,a);
+    	compute(n,n1,a,f);
+	output(n1,f);
 	return 0;
 }
